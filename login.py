2fb20d364e6955b0b4f2b7cd2c07d0f4f0988576
Andy <anwong16@gmail.com>
2024-05-08 16:48:39 -0700

    Added code to select login element. But need to wait for page to reload before we can proceed

--- login.py
+++ login.py
@@ -6,7 +6,23 @@
 # Verify user is logged in
 # Expected Results user is logged in
 
-from selenium import webdriver 
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+from selenium.webdriver.common.by import By
 
+# Step 1 Navigate to tracker.gg
+## We can definitely just open the login page directly but I want to explore the navigation functionality of selenium
+print('Openning firefox on page tracker.gg')
 driver = webdriver.Firefox()
 driver.get('https://tracker.gg/')
+
+# Step 2 Select login and click on it
+## 
+print('Scanning for login element')
+element_login = driver.find_element(By.XPATH,'/html/body/div/div/nav/div[3]/a[2]')
+element_login.send_keys(Keys.RETURN)
+
+element_login.find_element(By.NAME,'UserName')
+element_login.send_keys('bananatest')
+
+element_login.find_element(By.NAME,'Password')
