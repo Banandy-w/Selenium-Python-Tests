2a76cf0d787e5825bade07ea5aa8bea45b93a57a
Andy <anwong16@gmail.com>
2024-05-14 18:51:30 -0700

    removed locator variables for login_page.py

--- POM/pages/login_page.py
+++ POM/pages/login_page.py
@@ -6,9 +6,6 @@ class LoginPage:
     
     def __init__(self, driver):
         self.driver = driver
-        self.username_textbox = (By.CSS_SELECTOR, "input.trn-input:nth-child(1)")
-        self.password_textbox = (By.CSS_SELECTOR,'input.trn-input:nth-child(2)')
-        self.login_button = (By.CSS_SELECTOR,"button.trn-button")
 
     def input_username(self,username):
         self.driver.find_element(*Locator.USERNAME_TEXTBOX).send_keys(username)
