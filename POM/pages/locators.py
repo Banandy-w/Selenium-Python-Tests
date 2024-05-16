201a9482dd06ddd6d65bed00311b5cbd5ffa7bbd
Andy <anwong16@gmail.com>
2024-05-16 16:34:23 -0700

    Added USER_ICON locator to locators file

--- POM/pages/locators.py
+++ POM/pages/locators.py
@@ -4,4 +4,6 @@ class LoginPageLocators(object):
     USERNAME_TEXTBOX = (By.CSS_SELECTOR, "input.trn-input:nth-child(1)")
     PASSWORD_TEXTBOX = (By.CSS_SELECTOR,'input.trn-input:nth-child(2)')
     LOGIN_BUTTON = (By.CSS_SELECTOR,"button.trn-button")
-
+    USER_ICON = (By.CLASS_NAME, 'trn-game-bar-user')
+class SearchPageLocators (object):
+    ...
