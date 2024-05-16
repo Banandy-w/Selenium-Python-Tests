326f7edec0b472fb22450d7311ac8d47f58112cb
Andy <anwong16@gmail.com>
2024-05-16 16:35:41 -0700

    added documentation to login_page

--- POM/pages/login_page.py
+++ POM/pages/login_page.py
@@ -5,11 +5,17 @@ from POM.pages.home_page import BasePage
 
 class LoginPage(BasePage):
 
+    def __init__(self,driver):
+        super().__init__(driver)
+
+    """Inputs text into login textbox"""
     def input_username(self,username):
         self.driver.find_element(*Locator.USERNAME_TEXTBOX).send_keys(username)
     
+    """Inputs text into login textbox"""
     def input_password(self,password):
         self.driver.find_element(*Locator.PASSWORD_TEXTBOX).send_keys(password)
 
+    """Clicks on login button"""
     def click_login(self):
         self.driver.find_element(*Locator.LOGIN_BUTTON).click()
